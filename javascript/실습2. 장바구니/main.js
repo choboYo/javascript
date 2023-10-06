1fc1b28b54780a537ba66d6817255054a0a00c7c
choboYo <144123134+choboYo@users.noreply.github.com>
2023-10-06 21:44:46 +0900

    마지막

---
+++ javascript/실습2. 장바구니/main.js
@@ -0,0 +1,10 @@
+let button = document.querySelector('button');
+button.addEventListener('click', price);
+
+function price() {
+    document.querySelector('value');
+    document.querySelector('price');
+    let('value' + '*' + 'price.slice(-1)');
+    let result = ('value' + '*' + 'price.slice(-1)');
+    console.log(result)
+}
